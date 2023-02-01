86bcfe7a3932f0912ff815301eae7772e699dee5
Beatrice Abrahamsson <beaabrahamsson@hotmail.com>
2023-02-01 16:05:13 +0100

    rename project

--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -2,6 +2,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MVCProject.Controllers {
     public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
 
+        public IActionResult About() {
+            return View();
+        }
+
+        public IActionResult Books() {
+            return View();
+        }
     }
 }
